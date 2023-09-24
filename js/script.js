e85cff39b1065912ae838805b876abd679e7e30c
Vebster <vebster2580@gmail.com>
2023-09-24 22:32:52 +0300

    :

--- js/script.js
+++ js/script.js
@@ -801,3 +801,74 @@ const myNumber = 42
 // renderChart(chartContent);
 // renderChart(chartContent2);
 // renderChart(chartContent3);
+// // factory / constructor
+// function createUsers(name, age, sex) {
+//     return {
+//         name,
+//         age,
+//         sex
+//     };
+// }
+
+// const user1 = createUsers("Nikolas", "18", "male");
+
+// console.log(user1);
+// // constructer
+// function CreateUsers(name, age, sex) {
+//     this.name = name;
+//     this.age = age;
+//     this.sex = sex;
+// }
+
+// const user12 = new CreateUsers("Nikolas", "18", "male");
+
+// console.log(user12);
+
+// function LotteryCard(index, isWinning){
+//     this.index = index;
+//     this.isWinning = isWinning;
+//     this.handleTry = function (attempt, square, squareContainer) {
+//         console.log(attempt);
+//         if (this.isWinning){
+//             alert("Congrats! You have won");
+//             square.classList.add("square-correct");
+//         } else{
+//             if (attempt === 3 ){
+//                 alert(`Sorry you lost the game`);
+//                 squareContainer.innerHTML = null;
+//                 return;
+//             }
+//             alert("Try one more time");
+//             square.classList.add("square-wrong")
+//         }
+//     };
+// }
+// const lottoCards = [
+//     new LotteryCard(1, true),
+//     new LotteryCard(456790, false),
+//     new LotteryCard(2, false),
+//     new LotteryCard(5, false),
+//     new LotteryCard(14, false),
+//     new LotteryCard(46, false),
+//     new LotteryCard(44, false),
+//     new LotteryCard(8, false),
+//     new LotteryCard(1488, false),
+//     ];
+
+//     function renderSquares(squares){
+//         let attempt = 0;
+//         const squareContainer = document.querySelector(".squares-container")
+//         squares.forEach((item) => {
+//             const square = document.createElement("div");
+//             const squareIndex = document.createElement("h4");
+//             squareIndex.innerHTML = item.index;
+//             square.appendChild(squareIndex);
+//             square.classList.add("square");
+//             square.addEventListener("click", () => {
+//                 item.handleTry(++attempt, square, squareContainer);
+//             });
+//             squareContainer.appendChild(square);
+//         });
+//     }
+
+// renderSquares(lottoCards);
